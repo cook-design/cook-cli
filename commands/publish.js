f863de502075c6120eadc6d41fa5541b798b0b80
lizhen <lz@choicesoft.com.cn>
2019-04-25 16:51:49 +0800

    回滚，使用新特性支撑d.ts上传

--- commands/publish.js
+++ commands/publish.js
@@ -5,7 +5,7 @@ const { version } = require(path.join(process.cwd(), 'package.json'));
 
 function publish() {
   const cwdPath = path.join(process.cwd());
-  const shell = `cd ${cwdPath} && npm run lint && npm run compile && npm run copyds && git tag ${version} && git push origin ${version} && npm publish`;
+  const shell = `cd ${cwdPath} && npm run lint && npm run compile && git tag ${version} && git push origin ${version} && npm publish`;
   const ps = exec(shell, (err) => {
     if (err) {
       console.log(`Error: ${err}`) // eslint-disable-line
